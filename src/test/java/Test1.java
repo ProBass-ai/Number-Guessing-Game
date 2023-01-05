86c9ac1759cd73097888001907e541424c2c1281
prothedev <tebogoalbanlefatola@gmail.com>
2023-01-05 17:59:22 +0200

    IntroductoryTest Created

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -1,2 +1,38 @@
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.contrib.java.lang.system.SystemOutRule;
+
+
+import static org.junit.Assert.assertEquals;
+
+
 public class Test1 {
+
+    @Rule
+    public final SystemOutRule systemOutRule = new SystemOutRule().enableLog();
+
+
+    @Test
+    public void IntroductoryTextTest() {
+
+        String expectedText = """
+                **********************************************
+                 *** Welcome to the Number Guessing Game! ***\s
+                **********************************************
+                
+                Rules:
+                 * You have to guess the hidden number to win the game.
+                 * If you have 10 turns to guess the number.
+                 * Points will be awarded to you as play the game.
+                 * Type - 'docs' to find out how points will be awarded to you.
+                 * Each player data will be saved in database therefore, player identity will requested.
+                 * Enjoy!!!
+                 
+                """;
+
+        Main.main(new String[]{""});
+
+        assertEquals(expectedText, systemOutRule.getLog());
+    }
 }
